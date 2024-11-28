be278ac4ee673ad0548cdc6d3dd461499ddc51e3
Sammy maina <sammymainakirathe@gmail.com>
2024-11-28 11:33:01 +0300

    Made changes on navbar component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './pages/home/home.component';
 export const routes: Routes = [ 
     { path: 'home', component: HomeComponent },
     { path: 'menu', component: MenuComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
+    { path: 'category/:category', component: CategoryComponent }, // Dynamic route for categories
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
   
 ];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, RouterModule],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    RouterModule,
+  
+  ],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {}
+
+
+export class NavbarComponent {
+  
+}
